2c78e7eccbfa0dce54b0d0e759b5366c7de254fd
Omkar <omkarshirwadkar@wissen.com>
2025-02-13 16:46:32 +0530

    Changes

--- Maven_Tutorials/assignment/src/main/java/assignment/CalculatorMain.java
+++ Maven_Tutorials/assignment/src/main/java/assignment/CalculatorMain.java
@@ -3,7 +3,8 @@ package assignment;
 public class CalculatorMain{
     public static void main(String [] args){
         Calculator calc = new Calculator();
-        calc.add(10, 20);
-        calc.sub(20, 10);
+        System.out.println(calc.add(10, 20));
+        System.out.println(calc.sub(10, 20));
+        // calc.sub(20, 10);
     }
 }
