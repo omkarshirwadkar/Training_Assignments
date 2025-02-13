9126fe9ec4fc8a686f979152249a66aa1f0f38c3
Omkar <omkarshirwadkar@wissen.com>
2025-02-13 12:21:52 +0530

    Java with Jenkins

---
+++ Jenkins/ABC.java
@@ -0,0 +1,9 @@
+public class ABC{
+	public static void main(String args[]){
+		System.out.println("<<-------- ***WELCOME*** -------->>");
+		System.out.println("From ABC.java file");
+		System.out.println("Compiling and Executing through Jenkins");
+		System.out.println("From ABC.java file");
+		System.out.println("<<-------- ***THANK YOU*** -------->>");
+	}
+}
