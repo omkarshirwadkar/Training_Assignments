d7b1a992aca0e93069df7065f028f53452108b6b
Omkar <omkarshirwadkar@wissen.com>
2025-02-13 17:14:22 +0530

    Multiplication Statement removed

--- Maven_Tutorials/assignment/src/main/java/assignment/CalculatorMain.java
+++ Maven_Tutorials/assignment/src/main/java/assignment/CalculatorMain.java
@@ -5,6 +5,6 @@ public class CalculatorMain{
         Calculator calc = new Calculator();
         System.out.println( " Addition: " + calc.add(10, 20));
         System.out.println( " Subtraction: " + calc.sub(10, 20));
-        System.out.println( " Multiplication: " + calc.mul(10, 20));
+        // System.out.println( " Multiplication: " + calc.mul(10, 20));
     }
 }
