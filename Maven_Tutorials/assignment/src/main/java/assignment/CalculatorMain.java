8e6b26250560ecd85e6401c77214444ce0d0be74
Omkar <omkarshirwadkar@wissen.com>
2025-02-13 16:02:10 +0530

    Calculator Jenkins

---
+++ Maven_Tutorials/assignment/src/main/java/assignment/CalculatorMain.java
@@ -0,0 +1,9 @@
+package assignment;
+
+public class CalculatorMain{
+    public static void main(String [] args){
+        Calculator calc = new Calculator();
+        calc.add(10, 20);
+        calc.sub(20, 10);
+    }
+}
